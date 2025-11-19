59c107e07b86ada7982dd94b9f6b0e9dd3e603b4
theeuzin13 <matt.henrique13700@gmail.com>
2025-11-19 09:44:43 -0300

    feat: add categories module with controller, service, and DTOs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CategoriesModule } from './modules/categories/categories.module';
 
 @Module({
-  imports: [],
+  imports: [CategoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
