017621afd0473d44fab4735a625840c3343b8a8c
theeuzin13 <matt.henrique13700@gmail.com>
2025-12-01 10:46:51 -0300

    feat: update environment configuration and add login endpoint to Swagger documentation

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from 'src/common/entities/user.entity';
+import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  controllers: [],
+  imports: [
+    TypeOrmModule.forFeature([UserEntity]),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'secretpadrao',
+      signOptions: { expiresIn: parseInt(process.env.JWT_EXPIRATION ?? "3600") },
+    }),
+  ],
   providers: [AuthService],
+  controllers: [AuthController],
+  exports: [AuthService],
 })
 export class AuthModule {}
+
