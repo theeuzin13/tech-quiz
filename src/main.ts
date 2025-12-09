5a741d3fdebb5b481c7b682f76a4131e7fcfada7
root <root@DESKTOP-3Q0070J.>
2025-12-09 18:32:41 -0300

    feat:docker

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
+import * as crypto from 'crypto';
+(global as any).crypto = crypto;
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
