5279586fbf4a6b6d42e242699cb597b1848cf4fe
theeuzin13 <matt.henrique13700@gmail.com>
2025-12-15 16:10:20 -0300

    fix: remove unnecessary initialization calls and streamline seed functions

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,18 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    origin: (origin, callback) => {
+      const allowedOrigins = [
+        'https://app.seudominio.com',
+        'http://localhost:4200',
+      ];
+
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     credentials: true,
   });
 
